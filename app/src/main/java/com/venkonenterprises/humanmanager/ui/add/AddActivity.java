4f1da27966ec94d504759f8065486f1c4b2c54ff
Luis Vargas <luisev96@gmail.com>
2020-01-02 11:12:00 -0600

    Add activity added

---
+++ app/src/main/java/com/venkonenterprises/humanmanager/ui/add/AddActivity.java
@@ -0,0 +1,16 @@
+package com.venkonenterprises.humanmanager.ui.add;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.venkonenterprises.humanmanager.R;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
