c5aa2d1074636d10a606ceca95a9e831996379b9
Luis Vargas <luisev96@gmail.com>
2020-01-02 10:47:45 -0600

    Login Activity
    Initiate project

---
+++ app/src/main/java/com/venkonenterprises/humanmanager/ui/login/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.venkonenterprises.humanmanager.ui.login;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.venkonenterprises.humanmanager.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Context context = this;
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);*/
+            }
+        });
+    }
+}
